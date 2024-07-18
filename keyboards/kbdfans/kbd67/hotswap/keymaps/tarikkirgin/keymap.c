07f7d2ca98a4f52d438b719c90420f1fbb3f9680
Tarik Kirgin <hi@tarikkirgin.com>
2024-07-18 19:15:15 +0100

    add both shifts = caps lock

--- keyboards/kbdfans/kbd67/hotswap/keymaps/tarikkirgin/keymap.c
+++ keyboards/kbdfans/kbd67/hotswap/keymaps/tarikkirgin/keymap.c
@@ -4,6 +4,11 @@
 #define _______ KC_TRNS
 #define RHYP_T(kc) MT(MOD_RCTL | MOD_RSFT | MOD_RALT | MOD_RGUI, kc)
 
+const uint16_t PROGMEM both_shifts[] = {KC_LSFT, KC_RSFT, COMBO_END};
+combo_t key_combos[] = {
+    COMBO(both_shifts, KC_CAPS),
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [0] = LAYOUT(
   KC_ESC,  KC_1,    KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_GRV,  KC_PSCR,
@@ -16,7 +21,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, KC_F1 , KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_F13, KC_F14, _______,
   _______, KC_F15, KC_UP, KC_F16, KC_F17, KC_F18, _______, _______, _______, _______, _______, _______, _______, _______, QK_LOCK,
   _______,  KC_LEFT, KC_DOWN, KC_RIGHT, KC_F19, TG(2), _______, _______, _______, _______, _______, _______,    _______,  KC_SCRL,
-  KC_CAPS, KC_MPRV, KC_MPLY, KC_MNXT, _______, QK_BOOT, _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, KC_CAPS,  KC_INS, _______,
+  _______, KC_MPRV, KC_MPLY, KC_MNXT, _______, QK_BOOT, _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______,  KC_INS, _______,
   _______, _______, _______,                   KC_F21,                           _______, _______,   KC_HOME, KC_PAUS, KC_END),
 
 [2] = LAYOUT(
