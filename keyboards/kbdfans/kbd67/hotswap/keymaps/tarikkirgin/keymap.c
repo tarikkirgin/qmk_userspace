9da21f3903cc7a3d94d82cfd3a117947cd0f2b32
Tarik Kirgin <hi@tarikkirgin.com>
2024-09-30 13:25:11 +0100

    change hyper tap to esc

--- keyboards/kbdfans/kbd67/hotswap/keymaps/tarikkirgin/keymap.c
+++ keyboards/kbdfans/kbd67/hotswap/keymaps/tarikkirgin/keymap.c
@@ -10,14 +10,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,          KC_DEL,
   LCTL_T(KC_BSPC), KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT,       KC_ENT,      KC_PGUP,
   KC_LSFT, KC_Z,    KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH,       KC_RSFT,             KC_UP,   KC_PGDN,
-  KC_LGUI, KC_LALT, RHYP_T(KC_F20),                KC_SPC,                   KC_RALT, MO(1),                KC_LEFT, KC_DOWN, KC_RGHT),
+  KC_LGUI, KC_LALT, RHYP_T(KC_ESC),                KC_SPC,                   KC_RALT, MO(1),                KC_LEFT, KC_DOWN, KC_RGHT),
 
 [1] = LAYOUT(
   _______, KC_F1 , KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_F13, KC_F14, _______,
   _______, KC_F15, KC_UP, KC_F16, KC_F17, KC_F18, _______, _______, _______, _______, _______, _______, _______, _______, QK_LOCK,
   _______,  KC_LEFT, KC_DOWN, KC_RIGHT, KC_F19, TG(2), _______, _______, _______, _______, _______, _______,    _______,  KC_SCRL,
   _______, KC_MPRV, KC_MPLY, KC_MNXT, _______, QK_BOOT, _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______,  KC_INS, _______,
-  _______, _______, _______,                   KC_F21,                           _______, _______,   KC_HOME, KC_PAUS, KC_END),
+  _______, _______, _______,                   KC_F20,                           _______, _______,   KC_HOME, KC_PAUS, KC_END),
 
 [2] = LAYOUT(
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,
